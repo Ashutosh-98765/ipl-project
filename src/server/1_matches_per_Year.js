d805b42c5e1caa0eee3dfb1a8e59d2815720257a
Ashutosh-98765 <asshu65656@gmail.com>
2024-09-20 14:58:30 +0530

    modified matches per year file

--- src/server/1_matches_per_Year.js
+++ src/server/1_matches_per_Year.js
@@ -1,21 +1,38 @@
+// 1.Number of matches played per year for all the years in IPL.
+
 import fs from 'fs';
 import matches from '../data/matches.json' assert { type: 'json' }
 
+// function matchPerYear(){
+//   let result ={};
+//   for(let i=0; i<matches.length; i++){
+//     let season = matches[i]['season'];
+//     if(result[season]){
+//       result[season] = result[season]+1;
+//     }
+//     else{
+//       result[season]=1;
+//     }
+//   }
+//   return result;
+// }
 
-function matchPerYear(){
-  let result ={};
+
+
+function match_Per_Year(){
+  let matchPerYear ={};
   for(let i=0; i<matches.length; i++){
-    let season = matches[i]['season'];
-    if(result[season]){
-      result[season] = result[season]+1;
+    let { season } = matches[i];
+    if(matchPerYear[season]){
+      matchPerYear[season]++;
     }
     else{
-      result[season]=1;
+      matchPerYear[season]=1;
     }
   }
-  return result;
+  return matchPerYear;
 }
-let result = matchPerYear();
+let result = match_Per_Year();
 
 
 const jsonData = JSON.stringify(result, null, 2);
