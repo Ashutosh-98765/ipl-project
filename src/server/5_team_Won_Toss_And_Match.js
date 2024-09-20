5d0d722c3ec825c7d44517b01a58e0911588ff0d
Ashutosh-98765 <asshu65656@gmail.com>
2024-09-20 19:37:33 +0530

    modified teamWonTheTossAndWin file

--- src/server/5_team_Won_Toss_And_Match.js
+++ src/server/5_team_Won_Toss_And_Match.js
@@ -1,26 +1,45 @@
+// 5. Find the number of times each team won the toss 
+// and also won the match
+
 import fs from 'fs';
 import matches from '../data/matches.json' assert { type: 'json' }
 
 
 
-function teamWonTossAndMatch(){
-    let result = {};
-    for(let i=0; i<matches.length; i++){
-        let toss_winner = matches[i].toss_winner
-        let winner = matches[i].winner
-        if(toss_winner === winner){
-        if(result[winner]){
-            result[winner]++
-        }
-        else{
-            result[winner] = 1;
-        }
+// function teamWonTossAndMatch(){
+//     let result = {};
+//     for(let i=0; i<matches.length; i++){
+//         let toss_winner = matches[i].toss_winner
+//         let winner = matches[i].winner
+//         if(toss_winner === winner){
+//         if(result[winner]){
+//             result[winner]++
+//         }
+//         else{
+//             result[winner] = 1;
+//         }
+//     }
+//     }
+//     return result;
+// }
+
+function team_Won_Toss_And_Match(){
+  let teamWonTossAndMatch = {};
+  for(let i=0; i<matches.length; i++){
+    let { winner , toss_winner} = matches[i];
+    if(winner === toss_winner){
+      if(teamWonTossAndMatch[winner]){
+        teamWonTossAndMatch[winner]++;
+      }
+      else{
+        teamWonTossAndMatch[winner] = 1;
+      }
     }
-    }
-    return result;
+  }
+  return teamWonTossAndMatch
 }
 
-let result = teamWonTossAndMatch();
+let result = team_Won_Toss_And_Match();
 const jsonData = JSON.stringify(result, null, 2);
 
 try {
