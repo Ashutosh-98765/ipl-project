bb46d5f36c2009f876747d0b4aee39515edc6828
Ashutosh-98765 <asshu65656@gmail.com>
2024-09-20 19:39:08 +0530

    Added teamWonTheTossAndWin by using inbuilt function

---
+++ src/server/5_TeamWonTossAndMatch.js
@@ -0,0 +1,18 @@
+// 5. Find the number of times each team won the toss and also 
+// won the match
+
+import matches from '../data/matches.json' assert {type : 'json'};
+
+function teamWonTossAndMatch(){
+
+    let result = matches.reduce((acc , {toss_winner, winner}) => {
+        if(toss_winner === winner){
+            acc[winner] = (acc[winner] || 0) + 1;
+        }
+        return acc;
+    },{});
+
+    console.log(result);
+}
+
+teamWonTossAndMatch();
