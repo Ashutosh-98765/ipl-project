7a51b4331f5924f9733e45a98bee5e8484fff756
Ashutosh-98765 <asshu65656@gmail.com>
2024-09-21 14:46:35 +0530

    modified this file by using some proper function and destructuring

--- src/server/9_bowler_With_Best_SuperOver.js
+++ src/server/9_bowler_With_Best_SuperOver.js
@@ -2,41 +2,41 @@ import fs from 'fs';
 
 import deliveries from '../data/deliveries.json' assert { type:'json'}
 
-function bowlerWithBestInSuperOver(){
+function bowler_best_in_superOver(){
    
 
-        let economyBowlers = {};
+        let economy_bowlers = {};
     
         for (let i = 0; i < deliveries.length; i++) {
     
             const { is_super_over, bowler, total_runs, wide_runs, noball_runs } = deliveries[i];
     
             if (is_super_over !== "0") {
-                if (economyBowlers[bowler]) {
-                    economyBowlers[bowler]['total_runs'] += Number(total_runs);
+                if (economy_bowlers[bowler]) {
+                    economy_bowlers[bowler]['total_runs'] += Number(total_runs);
     
                     if (wide_runs === "0" && noball_runs === "0") {
-                        economyBowlers[bowler]['total_balls']++;
+                        economy_bowlers[bowler]['total_balls']++;
                     }
                 }
                 else {
-                    economyBowlers[bowler] = { 'total_runs': 0, 'total_balls': 0 };
-                    economyBowlers[bowler]['total_runs'] = Number(total_runs);
+                    economy_bowlers[bowler] = { 'total_runs': 0, 'total_balls': 0 };
+                    economy_bowlers[bowler]['total_runs'] = Number(total_runs);
     
                     if (wide_runs === "0" && noball_runs === "0") {
-                        economyBowlers[bowler]['total_balls']++;
+                        economy_bowlers[bowler]['total_balls']++;
                     }
                 }
             }
         }
     
-    let bestEconomyBowler = {};
+    let best_economy_bowler = {};
 
     let bowler = "";
     let eco = Number.MAX_VALUE;
-    for(let key in economyBowlers){
-        let totalRuns = economyBowlers[key]['total_runs']
-        let totalBall = economyBowlers[key]['total_balls']
+    for(let key in economy_bowlers){
+        let totalRuns = economy_bowlers[key]['total_runs']
+        let totalBall = economy_bowlers[key]['total_balls']
         let economy = totalRuns/(totalBall/6);
         if(eco > economy){
             eco = economy;
@@ -44,14 +44,14 @@ function bowlerWithBestInSuperOver(){
         }
     }
 
-    bestEconomyBowler[bowler] = eco;
-    return bestEconomyBowler;
+    best_economy_bowler[bowler] = eco;
+    return best_economy_bowler;
           
     
 }
     
 
-let result = bowlerWithBestInSuperOver();
+let result = bowler_best_in_superOver();
 const jsonData = JSON.stringify(result, null, 2);
 
 try {
