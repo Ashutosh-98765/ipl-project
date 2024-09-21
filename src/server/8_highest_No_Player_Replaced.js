90ab831f6b893a28358f96b1e8856c53c96d7e8b
Ashutosh-98765 <asshu65656@gmail.com>
2024-09-21 12:40:46 +0530

    modified highest_dismissed_player file by giving proper function name and use somewhere destructring

--- src/server/8_highest_No_Player_Replaced.js
+++ src/server/8_highest_No_Player_Replaced.js
@@ -4,53 +4,61 @@ import deliveries from '../data/deliveries.json' assert { type:'json'}
 
 
 
-function findHighestNoPlayerReplaced(){
-    let dismissData = {};
-    for(let key in deliveries){
-        let {player_dismissed : playerName , bowler:bowlerName} = deliveries[key]
-        if(playerName !== ""){
-            if(!dismissData[playerName]){
-                dismissData[playerName] = {}
-                if(!dismissData[playerName][bowlerName]){
-                    dismissData[playerName][bowlerName] = 1;
+function highest_dismissed_player() {
+
+    let dismissed_player_stats = {};
+
+    for (let i = 0; i < deliveries.length; i++) {
+
+        const { player_dismissed, bowler } = deliveries[i];
+
+        if (player_dismissed !== "") {
+            if (dismissed_player_stats[player_dismissed]) {
+                if (dismissed_player_stats[player_dismissed][bowler]) {
+                    dismissed_player_stats[player_dismissed][bowler]++;
                 }
-                else{
-                    dismissData[playerName][bowlerName]++;
+                else {
+                    dismissed_player_stats[player_dismissed][bowler] = 1;
                 }
             }
-            else{
-                if(!dismissData[playerName][bowlerName]){
-                    dismissData[playerName][bowlerName]=1;
-                }
-                else{
-                    dismissData[playerName][bowlerName]++;
-                }
+            else {
+                dismissed_player_stats[player_dismissed] = {};
+                dismissed_player_stats[player_dismissed][bowler] = 1;
+            }
+        }
+    }
+// console.log(dismissed_player_stats)
+
+
+    let highest_dismissal_player = {};
+
+    let playerName = "";
+    let bowlerName = "";
+    let max_count = 0;
+
+    for (let player in dismissed_player_stats) {
+
+        let player_stats = dismissed_player_stats[player];
+
+        for (let bowler in player_stats) {
+            if (player_stats[bowler] > max_count) {
+                max_count = player_stats[bowler];
+                bowlerName = bowler;
+                playerName = player;
             }
         }
     }
 
-    let result = {};
-    let max = 0;
-        let player = "";
-        let bowler = "";
-    for(let key in dismissData){
-        
-        for(let value in dismissData[key]){
-            if(dismissData[key][value]>max){
-                max = dismissData[key][value];
-                player = key;
-                bowler = dismissData[key];
-            }
-        }
+    highest_dismissal_player[playerName] = {};
+    highest_dismissal_player[playerName][bowlerName] = max_count;
 
-    }
-    result[player] = {};
-    result[player][bowler] = max;
-    return result;
+    return highest_dismissal_player;
+
 }
+// console.log(highest_dismissed_player());
 
-let result = findHighestNoPlayerReplaced();
-const jsonData = JSON.stringify(result, null, 2);
+let highest_dismissal_player = highest_dismissed_player();
+const jsonData = JSON.stringify(highest_dismissal_player, null, 2);
 
 try {
   fs.writeFileSync('/home/dell/JS-IPL-DATA-PROJECT/src/public/output/8_highest_No_Player_Replaced.json' , jsonData);
